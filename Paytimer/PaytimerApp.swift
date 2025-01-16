baa1619f687a03b4307b09ac052c3634db1e65e5
huguanhu <huguanhu@qimao.com>
2025-01-16 20:47:28 +0800

    Initial Commit

---
+++ Paytimer/PaytimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaytimerApp.swift
+//  Paytimer
+//
+//  Created by Hu on 2025/1/16.
+//
+
+import SwiftUI
+
+@main
+struct PaytimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PaytimerTests/PaytimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PaytimerTests.swift
+//  PaytimerTests
+//
+//  Created by Hu on 2025/1/16.
+//
+
+import Testing
+@testable import Paytimer
+
+struct PaytimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
